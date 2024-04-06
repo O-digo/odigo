a67a8f0b3f5d796969a1421abb06f74955bcc2f8
redcontroller <jasinis102@gmail.com>
2024-04-07 05:58:15 +0900

    Main UI 가짜 데모 기능 추가

--- FE/odigo-app/components/Control/ControlButton.js
+++ FE/odigo-app/components/Control/ControlButton.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
-export default function ControlButton({onPress}) {
+export default function ControlButton({onPress, mode}) {
+    let disalbed = (mode === 'disable' ? true : false);
+
     return (
-        <Container onPress={onPress} underlayColor='#fff' >
-            <ControlButtonText>시작하기</ControlButtonText>
+        <Container onPress={onPress} underlayColor='#fff' mode={mode} disabled={disalbed}>
+            <ControlButtonText mode={mode}>{mode !== 'quit' ? '시작하기' : '종료하기'}</ControlButtonText>
         </Container>
     )
 }
 
 const Container = styled.TouchableOpacity`
     margin: 16px 20px;
-    background-color: rgb(44, 44, 54);
+    background-color: ${({mode}) => mode === 'disable' ? '#202027' : mode === 'start' ? '#3182F6' : '#F83242'};
     /* border: 2px solid rgb(49, 130, 246); */
     border-radius: 16px;
     position: fixed;
@@ -19,7 +21,7 @@ const Container = styled.TouchableOpacity`
 `;
 
 const ControlButtonText = styled.Text`
-    color: #7E7E87;
+    color: ${({mode})=> mode === 'disable' ? '#7E7E87' : '#FFFFFF' };
     font-size: 18px;
     margin: 21.5px 14.5px;
     text-align: center;
