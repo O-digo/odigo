17c98d1fa1d87416db0adfc6a24bf25926b6094e
redcontroller <jasinis102@gmail.com>
2024-04-06 17:12:00 +0900

    Main 페이지 UI 개발 중

---
+++ FE/odigo-app/App.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Main from './screens/Main.js';
+import Search from './screens/Search.js';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="Search" component={Search} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ FE/odigo-app/screens/Main.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Alert, View, Text, Button } from 'react-native';
+import styled from 'styled-components/native';
+import imgStation1 from '../assets/img/Frame_15.png';
+
+function Screen1({ navigation }) {
+    const [currentStation, setCurrentStation] = useState({station:"영등포", no:1});
+
+    const onPressButton = () => {
+        Alert.alert("준비중입니다.");
+    }
+
+    return (
+    <StyledView>
+        <StyledTitle>이번 역</StyledTitle>
+        <Button title="영등포" onPress={onPressButton} />
+        <StyledTitle>목적 역</StyledTitle>
+        <Button title="선택하기" onPress={() => navigation.navigate('Search')} />
+        <Text>자주 가는 역</Text>
+        <Button title="추가하기 +" onPress={onPressButton} />
+        <Button title="시작하기" onPress={onPressButton} />
+    </StyledView>
+    );
+}
+
+const StyledView = styled.View`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    background-color: black;
+`;
+
+const StyledTitle = styled.Text`
+    font-size: 20px;
+    color: white;
+`;
+
+
+export default Screen1;
