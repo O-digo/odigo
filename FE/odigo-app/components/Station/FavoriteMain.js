fb5e056eb040ae8f3b7b9e1b603ea93885cb0215
윤태웅 <omygog@naver.com>
2024-04-08 17:32:09 +0900

    fix: MainFavoriteButton Error fix

--- FE/odigo-app/components/Station/FavoriteMain.js
+++ FE/odigo-app/components/Station/FavoriteMain.js
@@ -4,15 +4,15 @@ import FavoriteStation from './FavoriteStation.js';
 import AddFavorite from './AddFavorite.js';
 import FavoriteList from '../SearchScreen/FavoriteList';
 
-export default function FavoriteMain({onEdit, onAdd, favorite}) {
-    return (
-        <View>
-            <FavoriteStation onPress={onEdit} />
-            {
-                favorite.length
-                    ? <FavoriteList />
-                    :<AddFavorite onPress={onAdd} />
-            }
-        </View>
-    );
+export default function FavoriteMain({ onEdit, onAdd, favorite }) {
+  return (
+    <View>
+      <FavoriteStation onPress={onEdit} />
+      {favorite.length ? (
+        <FavoriteList favoriteItems={favorite} />
+      ) : (
+        <AddFavorite onPress={onAdd} />
+      )}
+    </View>
+  );
 }
